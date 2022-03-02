61b0b21076c1cf17a3cddac82cbc6916499252e7
Justin <justin@akohub.com>
2022-03-02 16:22:23 +0800

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screen: {
+      lg: "1280px",
+    },
+    container: {
+      padding: {
+        DEFAULT: "16px",
+        sm: "32px",
+      },
+    },
+    colors: {
+      // TODO:
+      primary: {},
+      gray: {
+        900: "#000000",
+      },
+      white: colors.white,
+    },
+    fontFamily: {
+      // TODO:
+      sans: ["Noto Sans TC", ...defaultTheme.fontFamily.sans],
+      // mono: defaultTheme.fontFamily.mono,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
